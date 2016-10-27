f5a32831700d48d385769a4a84839611b0f334a7
jinsedeyuzhou <jinsedeyuzhou@gamil.com>
2016-10-27 17:52:05 +0800

    sample

---
+++ videoplay/src/main/java/com/android/videoplayer/activity/MainActivity.java
@@ -0,0 +1,77 @@
+package com.android.videoplayer.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.videoplayer.R;
+import com.android.videoplayer.media.AndroidMediaController;
+import com.android.videoplayer.media.IjkVideoView;
+
+import tv.danmaku.ijk.media.player.IMediaPlayer;
+import tv.danmaku.ijk.media.player.IjkMediaPlayer;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private IjkVideoView mVideoView;
+    private String videoPath = "http://gslb.miaopai.com/stream/4YUE0MlhLclpX3HIeA273g__.mp4?yx=&refer=weibo_app";
+    private Button mBtnIJK;
+    private Button mBtnSurface;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+
+//        Settings settings = new Settings(getApplicationContext());
+        mVideoView = (IjkVideoView) findViewById(R.id.videoView);
+
+        AndroidMediaController mMediaController = new AndroidMediaController(this, false);
+
+        IjkMediaPlayer.loadLibrariesOnce(null);
+        IjkMediaPlayer.native_profileBegin("libijkplayer.so");
+
+        mVideoView.setMediaController(mMediaController);
+//        ijkVideo.setVideoURI(Uri.parse(videoPath));
+        mVideoView.setVideoPath(videoPath);
+        mVideoView.setOnPreparedListener(new IMediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(IMediaPlayer mp) {
+                mVideoView.start();
+            }
+        });
+//        mVideoView.start();
+    }
+
+    private void initView() {
+
+        mBtnIJK = (Button) findViewById(R.id.btn_ijk);
+        mBtnIJK.setOnClickListener(this);
+
+        mBtnSurface = (Button) findViewById(R.id.btn_surfaceview);
+        mBtnSurface.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = null;
+
+        System.out.print("执行了没");
+        int i = view.getId();
+        if (i == R.id.btn_ijk) {
+            intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+
+        } else if (i == R.id.btn_surfaceview) {
+            System.out.print("执行了没");
+            intent = new Intent(getApplicationContext(), SurfaceActivity.class);
+            startActivity(intent);
+
+        }
+    }
+}
---
+++ videoplay/src/main/java/com/android/videoplayer/activity/SurfaceActivity.java
@@ -0,0 +1,155 @@
+package com.android.videoplayer.activity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+import com.android.videoplayer.R;
+import com.android.videoplayer.config.VideoIjkBean;
+import com.android.videoplayer.media.VPlayPlayer;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Berkeley on 10/24/16.
+ */
+public class SurfaceActivity extends FragmentActivity implements OnClickListener {
+    private ArrayList<VideoIjkBean> videoBeens;
+    private VPlayPlayer vPlayPlayer;
+    private int index;
+    private String url="http://119.90.25.48/record2.a8.com/mp4/1477100428026014.mp4";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_surface);
+        videoBeens = new ArrayList<>();
+
+        VideoIjkBean videoBean = new VideoIjkBean();
+        videoBean.setTitle("遛狗");
+        videoBean.setStreamUrl("http://119.90.25.48/record2.a8.com/mp4/1477100428026014.mp4");
+        videoBeens.add(videoBean);
+
+        VideoIjkBean videoBean1 = new VideoIjkBean();
+        videoBean1.setTitle("wuliao");
+        videoBean1.setStreamUrl("http://119.90.25.48/record2.a8.com/mp4/1476696896120409.mp4");
+        videoBeens.add(videoBean1);
+
+        VideoIjkBean videoBean2 = new VideoIjkBean();
+        videoBean2.setTitle("shabi");
+        videoBean2.setStreamUrl("http://119.90.25.48/record2.a8.com/mp4/1476440343435698.mp4");
+        videoBeens.add(videoBean2);
+
+
+        vPlayPlayer = new VPlayPlayer(this);
+        vPlayPlayer.onError(new VPlayPlayer.OnErrorListener() {
+            @Override
+            public void onError(int what, int extra) {
+                Toast.makeText(getApplicationContext(), "video play error", Toast.LENGTH_SHORT).show();
+
+            }
+        }).onInfo(new VPlayPlayer.OnInfoListener() {
+            @Override
+            public void onInfo(int what, int extra) {
+
+            }
+        }).onComplete(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), "video play completed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        findViewById(R.id.play).setOnClickListener(this);
+        findViewById(R.id.pause).setOnClickListener(this);
+        findViewById(R.id.forward).setOnClickListener(this);
+        findViewById(R.id.back).setOnClickListener(this);
+        findViewById(R.id.fullscreen).setOnClickListener(this);
+        findViewById(R.id.toggleratis).setOnClickListener(this);
+        findViewById(R.id.start).setOnClickListener(this);
+        findViewById(R.id.source).setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int i = view.getId();
+        if (i == R.id.play) {
+            vPlayPlayer.play(url);
+            vPlayPlayer.setTitle(url);
+            vPlayPlayer.setShowNavIcon(true);
+
+
+        } else if (i == R.id.start) {
+            vPlayPlayer.start();
+
+
+        } else if (i == R.id.pause) {
+            vPlayPlayer.pause();
+
+        } else if (i == R.id.forward) {
+            vPlayPlayer.forward(+0.2f);
+
+        } else if (i == R.id.back) {
+            vPlayPlayer.forward(-0.2f);
+
+        } else if (i == R.id.toggleratis) {
+            vPlayPlayer.toggleAspectRatio();
+
+
+        } else if (i == R.id.fullscreen) {
+            vPlayPlayer.toggleFullScreen();
+
+        } else if (i == R.id.source) {
+            url = "http://119.90.25.48/record2.a8.com/mp4/1476440343435698.mp4";
+
+        }
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (vPlayPlayer != null) {
+            vPlayPlayer.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (vPlayPlayer != null) {
+            vPlayPlayer.onResume();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (vPlayPlayer != null) {
+            vPlayPlayer.onDestroy();
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (vPlayPlayer != null) {
+            vPlayPlayer.onConfigurationChanged(newConfig);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (vPlayPlayer != null && vPlayPlayer.onBackPressed()) {
+            return;
+        }
+        super.onBackPressed();
+    }
+}
