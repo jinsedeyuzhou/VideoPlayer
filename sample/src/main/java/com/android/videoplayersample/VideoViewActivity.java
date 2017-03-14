f7775e61bb422aa3481eb7afeb6437685467ac95
unknown <jinsedeyuzhou>
2017-03-15 00:10:35 +0800

    png

--- sample/src/main/java/com/android/videoplayersample/VideoViewActivity.java
+++ sample/src/main/java/com/android/videoplayersample/VideoViewActivity.java
@@ -14,29 +14,14 @@ import com.github.jinsedeyuzhou.ijkplayer.play.VPlayPlayer;
  */
 public class VideoViewActivity extends FragmentActivity {
 
-    private FrameLayout layout_video;
     private VPlayPlayer player;
-    private int mporit;
-    private int initHeight;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
         player = (VPlayPlayer) findViewById(R.id.layout_video);
-//        initHeight=layout_video.getLayoutParams().height;
-//        if (player == null)
-//            player =new VPlayPlayer(this);
-//        if (player.getParent() != null)
-//            ((ViewGroup) player.getParent()).removeAllViews();
         player.play("http://gslb.miaopai.com/stream/4YUE0MlhLclpX3HIeA273g__.mp4?yx=&refer=weibo_app");
-//        layout_video.addView(player);
-//        vPlayPlayer.setViewHeight(600);
-//        player.start("http://119.90.25.48/record2.a8.com/mp4/1476696896120409.mp4");
-
-//        mporit=layout_video.getLayoutParams().height;
-
-
     }
 
     @Override
@@ -80,20 +65,5 @@ public class VideoViewActivity extends FragmentActivity {
         if (player != null) {
             player.onConfigurationChanged(newConfig);
         }
-//        if (newConfig.orientation==Configuration.ORIENTATION_PORTRAIT)
-//        {
-//            ViewGroup.LayoutParams params = layout_video.getLayoutParams();
-//            int widthPixels = getResources().getDisplayMetrics().widthPixels;
-//            params.height = initHeight;
-//            layout_video.setLayoutParams(params);
-//        }else
-//        {
-//            ViewGroup.LayoutParams params = layout_video.getLayoutParams();
-//            int heightPixels =getResources().getDisplayMetrics().heightPixels;
-//            int widthPixels = getResources().getDisplayMetrics().widthPixels;
-//            params.height=widthPixels;
-////            params.width=widthPixels;
-//            layout_video.setLayoutParams(params);
-//        }
     }
 }
