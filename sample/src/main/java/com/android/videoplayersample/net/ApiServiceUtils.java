fcaae3450c56f6706e43d31165f59998d31220d4
jinsedeyuzhou <jinsedeyuzhou@gmail.com>
2017-05-12 21:38:34 +0800

    update

--- sample/src/main/java/com/android/videoplayersample/net/ApiServiceUtils.java
+++ sample/src/main/java/com/android/videoplayersample/net/ApiServiceUtils.java
@@ -17,24 +17,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 /**
- * ========================================
- * <p/>
- * 版 权：dou361.com 版权所有 （C） 2015
- * <p/>
- * 作 者：陈冠明
- * <p/>
- * 个人网站：http://www.dou361.com
- * <p/>
- * 版 本：1.0
- * <p/>
- * 创建日期：2016/3/7
- * <p/>
- * 描 述：具体的网络接口请求，分阻塞线程和异步线程
- * <p/>
- * <p/>
- * 修订历史：
- * <p/>
- * ========================================
  */
 public class ApiServiceUtils {
 
